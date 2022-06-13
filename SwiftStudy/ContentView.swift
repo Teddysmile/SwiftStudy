364673c2d79e38de7636e43a82f29531a12442ee
xiaoxiong <1053341795@qq.com>
2022-06-14 00:54:53 +0800

    Initial Commit

---
+++ SwiftStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftStudy
+//
+//  Created by ByteDance on 2022/6/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftStudy/SwiftStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStudyApp.swift
+//  SwiftStudy
+//
+//  Created by ByteDance on 2022/6/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
